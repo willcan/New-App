23c4fc56981db7ed14a45490227e23c6ae9414c0
willcan <59585577+willcan@users.noreply.github.com>
2020-01-08 16:09:19 -0700

    Updated Firebase Config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCIAewmtbTiO8XSX_ZLl7u3aVeR8gAEaK4",
+  authDomain: "new-app-c7abb.firebaseapp.com",
+  databaseURL: "https://new-app-c7abb.firebaseio.com",
+  projectId: "new-app-c7abb",
+  storageBucket: "new-app-c7abb.appspot.com",
+  messagingSenderId: "581040163869",
+  appId: "1:581040163869:web:4635a159254bb7aee94af4"
 };
 
 firebase.initializeApp(config);
